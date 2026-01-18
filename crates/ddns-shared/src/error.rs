67e8d43b5a158765e03f9b3b90a4a8c67f5e2c58
derrick921213 <dlin12457@gmail.com>
2026-01-18 14:20:14 +0800

    Add: Provider code

--- crates/ddns-shared/src/error.rs
+++ crates/ddns-shared/src/error.rs
@@ -14,10 +14,28 @@ pub enum DdnsServerError {
     ServerStartError(String),
     #[error("使用者管理錯誤: {0}")]
     UserManagementError(String),
+    #[error("提供者錯誤: {0}")]
+    DnsProviderError(#[from] DnsError),
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum DdnsClientError {
     #[error("客戶端錯誤: {0}")]
     ClientError(String),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum DnsError {
+    #[error("unauthorized")]
+    Unauthorized,
+    #[error("resource not found")]
+    NotFound,
+    #[error("rate limited")]
+    RateLimited,
+    #[error("invalid request: {0}")]
+    Invalid(String),
+    #[error("provider error: {0}")]
+    Provider(String),
+    #[error("transient error: {0}")]
+    Transient(String),
 }
